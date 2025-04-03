7f51f88eded296180f7faa2afd1925c8c7297dfb
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2025-04-03 13:12:06 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,48 @@
+export const tasks = [
+    {
+        "id": "1",
+        "title": "Выучить JScript",
+        "status": "backlog",
+    },
+    {
+        "id": "2",
+        "title": "Выучить React",
+        "status": "backlog",
+    },
+    {
+        "id": "3",
+        "title": "Сделать д/з",
+        "status": "backlog",
+    },
+    {
+        "id": "4",
+        "title": "Выпить смузи",
+        "status": "processing",
+    },
+    {
+        "id": "5",
+        "title": "Выпить лекарство",
+        "status": "processing",
+    },
+    {
+        "id": "6",
+        "title": "Позвонить родителям",
+        "status": "done",
+    },
+    {
+        "id": "7",
+        "title": "Убраться дома",
+        "status": "done",
+    },
+    {
+        "id": "8",
+        "title": "Сходить погулять",
+        "status": "basket",
+    },
+    {
+        "id": "9",
+        "title": "Прочитать Войну и Мир",
+        "status": "basket",
+    },
+
+]
